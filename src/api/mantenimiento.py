ad46da7d8ac0cc492cccd2e86db437f672856b4a
Daniel-Soria190 <msdaniel190@gmail.com>
2026-05-06 15:53:54 -0600

    Feature: Endpoint equipos protegido

--- src/api/mantenimiento.py
+++ src/api/mantenimiento.py
@@ -6,28 +6,17 @@ from src.services.mantenimiento_service import set_mantto,search,get_Mtto,update
 from src.models.auth import TokenResponse
 from typing import Optional
 from datetime import datetime
+from src.services.auth_service import require_role
 
 router = APIRouter(prefix="/mantenimientos", tags=["mantenimientos"])
-# Esta ocurriendo un erro en esta parte, muy probablemente con los formatos del datetime.
 
-#@router.get ( "/")
-#async def buscar_mantenimiento (
-#    idOrden:Optional[int]= Query(None),
-#    tipo:Optional[str]=Query(None),
-#    Fi:Optional[datetime]=Query(None),
-#    Ff:Optional[datetime]=Query(None),
-#    rPor:Optional[int]=Query(None),
-#    vPor:Optional[int]=Query(None),
-#    externo:Optional[bool]=Query(None),
-#    rPorExt:Optional[int]=Query(None)    
-#    ):
-#    return await search(idOrden,tipo,Fi,Ff,rPor,vPor,externo,rPorExt)
 
 @router.get ( "/")
 async def buscar_mantenimiento (
     filters: MattoParams = Depends(), 
     limit: int = Query(10, ge=1, le=50), # Validamos min 1, max 50
-    page: int = Query(1, ge=1)           # Página actual
+    page: int = Query(1, ge=1),           # Página actual
+    current_user: dict = Depends(require_role(3))
 ):
     # Calculamos el offset (ej: página 1 -> offset 0, página 2 -> offset 10)
     offset = (page - 1) * limit
@@ -39,17 +28,23 @@ async def buscar_mantenimiento (
 
 
 @router.get("/{idMantenimiento}")
-async def obtener_mantenimiento(idMantenimiento:int):
+async def obtener_mantenimiento(idMantenimiento:int,
+        current_user: dict = Depends(require_role(3))
+        ):
     return await get_Mtto(idMantenimiento)
 
 
 @router.patch ("/{idMantenimiento}")
 async def update_matto(idMantenimiento:int ,
-                       filter:MattoUpdate=Depends()):
+                       filter:MattoUpdate=Depends(),
+                       current_user: dict = Depends(require_role(3))
+                       ):
     data=filter.model_dump(exclude_none=True)
     return await update(idMantenimiento,data)
 
 
 @router.post("/")
-async def mantenimiento( mantto:MattoRequest):
+async def mantenimiento( mantto:MattoRequest,
+        current_user: dict = Depends(require_role(3))
+                        ):
     return await set_mantto(mantto)
