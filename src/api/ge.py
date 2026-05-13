f43f21b0d156e584968a580df6b28b12ae9b8183
Daniel-Soria190 <msdaniel190@gmail.com>
2026-05-12 20:02:21 -0600

    FIX:Error 307 en GE corregido

--- src/api/ge.py
+++ src/api/ge.py
@@ -8,33 +8,33 @@ from src.models.auth import TokenResponse
 
 router = APIRouter(prefix="/GE", tags=["GE"])
 
-@router.get("/Area/",
+@router.get("/Area",
             response_model=list[AreaResponse],
             summary="Areas")    
 async def funcion ():
     return await get_area()
 
-@router.get("/GeFuncion/",
+@router.get("/GeFuncion",
             response_model=list[funcionResponse],
             summary="Funciones del equipo")    
 async def funcion ():
     return await get_funcion()
 
-@router.get("/GeRiesgo/",
+@router.get("/GeRiesgo",
             response_model=list[riesgoResponse],
             summary="Riesgo del equipo")    
 
 async def Riesgo ():
     return await get_riesgo()
 
-@router.get("/GeReqMto/",
+@router.get("/GeReqMto",
             response_model=list[ReqMtoResponse],
             summary="Requisitos de mantenimieto")    
 
 async def ReqMto ():
     return await get_ReqMto()
 
-@router.get("/GeAntecedentes/",
+@router.get("/GeAntecedentes",
             response_model=list[antecedenteResponse],
             summary="Antecedentes de fallo")    
 async def Antecedentes ():
